655649cbe2b1d3a062f0de006180c9367d69e3c8
Cole Duclos <cole.duclos@autodesk.com>
2018-04-22 09:04:14 -0700

    adding gcs pubsub subscription

--- infrastructure/modules/pubsub/main.tf
+++ infrastructure/modules/pubsub/main.tf
@@ -8,3 +8,8 @@ resource "google_pubsub_subscription" "twitter_stream_bigquery_subscription" {
     name  = "${data.google_project.project.name}_twitter_stream_bigquery_subscription"
     topic = "${google_pubsub_topic.twitter_stream_topic.name}"
 }
+
+resource "google_pubsub_subscription" "twitter_stream_gcs_subscription" {
+    name  = "${data.google_project.project.name}_twitter_stream_gcs_subscription"
+    topic = "${google_pubsub_topic.twitter_stream_topic.name}"
+}
